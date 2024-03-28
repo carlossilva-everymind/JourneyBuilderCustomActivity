21c4ffc4e85226a2616e8652512a53caf9f9e721
carlossilva-everymind <carlos.silva@everymind.com.br>
2024-03-27 22:54:30 -0300

    1

--- routes/activity.js
+++ routes/activity.js
@@ -2,6 +2,7 @@ const { v1: Uuidv1 } = require('uuid');
 const JWT = require('../utils/jwtDecoder');
 const SFClient = require('../utils/sfmc-client');
 const logger = require('../utils/logger');
+const https = require('https');
 
 /*
   Arquivo de configuração das rotas do backend da atividade customizada
@@ -22,6 +23,38 @@ exports.execute = async (req, res) => {
 
   try {
     const id = Uuidv1();
+
+    const postData = JSON.stringify({
+      "client_id": "adbb826a4a4a4f1f955d91125f066d65",
+      "client_secret": "B2fb771Fee944f8DB3B6D18e284f528d",
+      "grant_type": "CLIENT_CREDENTIALS"
+    });
+
+    const options = {
+      hostname: 'https://oauth-app-hml.br-s1.cloudhub.io',
+      path: '/token',
+      method: 'POST'
+    };
+
+    const req = https.request(options, res => {
+      let data = '';
+
+      res.on('data', chunk => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        console.log('data', data);
+      });
+    });
+
+    req.on('error', error => {
+      console.error('error', error);
+    });
+
+    console.log('postdata', postData);
+    req.write(postData);
+    req.end();
 
     await SFClient.saveData(process.env.DATA_EXTENSION_EXTERNAL_KEY, [
       {
