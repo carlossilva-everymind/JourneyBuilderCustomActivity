b222cf0d8b6d61bc42ed111ddc13cb0ff67dc35a
carlossilva-everymind <carlos.silva@everymind.com.br>
2024-03-29 22:44:42 -0300

    1

--- routes/activity.js
+++ routes/activity.js
@@ -96,16 +96,7 @@ exports.execute = async (req, res) => {
     ])
   } catch (error) {
     if (!sfmcToken) {
-      await axios.post(`https://${process.env.SFMC_SUBDOMAIN}.auth.marketingcloudapis.com/v2/token`,
-        {
-          "client_id": process.env.SFMC_CLIENT_ID,
-          "client_secret": process.env.SFMC_CLIENT_SECRET,
-          "grant_type": "client_credentials",
-          "account_id": process.env.SFMC_ACCOUNT_ID
-        }
-      ).then(response => {
-        console.log('sfmc token response:', response);
-      }).catch(error => console.log('sfmc token error:', error))
+      sfmcToken = await SFMCClient2.getToken();
     }
 
     logger.error(error);
