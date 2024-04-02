9aa0df0e4ae43f1f4087a9e48af12f836e0a71be
carlossilva-everymind <carlos.silva@everymind.com.br>
2024-04-02 20:31:30 -0300

    1

--- routes/activity.js
+++ routes/activity.js
@@ -60,7 +60,25 @@ exports.execute = async (req, res) => {
     console.log('post body motion: ', postBody);
     let responseMotion = await axios('https://proxy-motion-hml.br-s1.cloudhub.io/appointment/' + idAgendamento,
       postBody
-    );
+    )
+      .then(response => {
+        console.log('Response:', response.data);
+      })
+      .catch(error => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.error('Response data:', error.response.data);
+          console.error('Response status:', error.response.status);
+          console.error('Response headers:', error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.error('Request:', error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.error('Error:', error.message);
+        }
+      });
 
     // atualiza dados na DE
     await SFClient.saveDataByID(data.inArguments[0].dataExtensionID, [
